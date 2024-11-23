57708a80938bda16cd8a58ec4e07c5198e7aec82
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2024-11-23 07:45:09 +0300

    "Customize the admin change list"
    https://docs.djangoproject.com/en/5.1/intro/tutorial07/#customize-the-admin-change-list

--- src/polls/admin.py
+++ src/polls/admin.py
@@ -14,6 +14,9 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]  # noqa: RUF012
+    list_display = ["question_text", "pub_date", "was_published_recently"]  # noqa: RUF012
+    list_filter = ["pub_date"]  # noqa: RUF012
+    search_fields = ["question_text"]  # noqa: RUF012
 
 
 admin.site.register(Question, QuestionAdmin)
