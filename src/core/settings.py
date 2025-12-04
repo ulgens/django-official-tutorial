138a3cecebcbdfe5b8082217628425a28b9c5ab5
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2025-12-04 03:30:04 +0300

    Apply django-upgrade fixes

--- src/core/settings.py
+++ src/core/settings.py
@@ -107,11 +107,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 INTERNAL_IPS = ["127.0.0.1", "::1"]
 
 
